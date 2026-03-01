35bea86a27f43f0ffd73c6cf28890ad8e2527956
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-03-02 00:51:30 +0100

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,7 @@
+cd $(go list -f '{{ .Dir }}' github.com/google/certificate-transparency-go); \
+go install github.com/golang/mock/mockgen; \
+go install google.golang.org/protobuf/proto; \
+go install google.golang.org/protobuf/cmd/protoc-gen-go; \
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc; \
+go install github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc; \
+go install golang.org/x/tools/cmd/stringer
