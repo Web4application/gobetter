6396e0979a670c6ce49767ff7663bcf1c1ccc52b
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-03-02 01:02:28 +0100

    Update Install.go

--- Install.go
+++ Install.go
@@ -5,3 +5,4 @@ go install google.golang.org/protobuf/cmd/protoc-gen-go; \
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc; \
 go install github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc; \
 go install golang.org/x/tools/cmd/stringer
+go install github.com/web4application/gobetter@latest
